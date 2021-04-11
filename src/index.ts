be8c53602be6e066520fc2b4ba405c4c9b5e4d3e
kobakakzu0429 <kobakazu10@gmail.com>
2021-04-11 12:02:29 +0900

    add babel-runtime/regenerator

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
+import "babel-runtime/regenerator";
 import * as core from "./core";
-import * as enzyme from "./enzyme";
 import * as prettify from "./prettify";
 
-export { core, enzyme, prettify };
+export { core, prettify };
